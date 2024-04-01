df08fe73b7d08beffb80f7c0dd4dba6539956a4c
dee-mee <derekmuriuki2@gmail.com>
2024-04-01 12:43:23 +0300

    added a quantity bar

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ function addTask() {
 
     taskInput.value = "";
 }
+function addTask() {
+    let input = document.getElementById('taskInput');
+    let taskList = document.getElementById('taskList');
+
+    // Create a new task item
+    let taskItem = document.createElement('li');
+    let taskName = document.createTextNode(input.value);
+    taskItem.appendChild(taskName);
+
+    // Add a percentage input
+    let percentageInput = document.createElement('input');
+    percentageInput.type = 'number';
+    percentageInput.min = '0';
+    percentageInput.max = '100';
+    percentageInput.value = '0';
+    percentageInput.style.marginLeft = '10px';
+    taskItem.appendChild(percentageInput);
+
+    // Add the task item to the list
+    taskList.appendChild(taskItem);
+
+    // Clear the input field
+    input.value = '';
+}
