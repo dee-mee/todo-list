406b291093cd36575df6b9107155103c855d919e
dee-mee <derekmuriuki2@gmail.com>
2024-04-01 13:06:24 +0300

    added a delete button

--- script.js
+++ script.js
@@ -1,40 +1,14 @@
 function addTask() {
-    var taskInput = document.getElementById("taskInput");
-    var taskList = document.getElementById("taskList");
-
-    if (taskInput.value === "") {
-        alert("Please enter a task!");
-        return;
-    }
-
-    var li = document.createElement("li");
-    li.textContent = taskInput.value;
-
-    taskList.appendChild(li);
-
+    var taskInput = document.getElementById('taskInput');
+    var taskList = document.getElementById('taskList');
+    var newTaskDiv = document.createElement('div');
+    taskList.appendChild(newTaskDiv);
+    newTaskDiv.id = 'task' + (taskList.childElementCount + 1);
+    newTaskDiv.innerHTML = "<span id='task'>" + taskInput.value + "</span> <button onclick='deleteTask(\"" + newTaskDiv.id + "\")'>Delete</button>";
     taskInput.value = "";
-}
-function addTask() {
-    let input = document.getElementById('taskInput');
-    let taskList = document.getElementById('taskList');
-
-    // Create a new task item
-    let taskItem = document.createElement('li');
-    let taskName = document.createTextNode(input.value);
-    taskItem.appendChild(taskName);
-
-    // Add a percentage input
-    let percentageInput = document.createElement('input');
-    percentageInput.type = 'number';
-    percentageInput.min = '0';
-    percentageInput.max = '100';
-    percentageInput.value = '0';
-    percentageInput.style.marginLeft = '10px';
-    taskItem.appendChild(percentageInput);
-
-    // Add the task item to the list
-    taskList.appendChild(taskItem);
-
-    // Clear the input field
-    input.value = '';
-}
+  }
+  
+  function deleteTask(taskId) {
+    var task = document.getElementById(taskId);
+    task.parentNode.removeChild(task);
+  }
