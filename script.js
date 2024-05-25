788e5fc36f58a32c3a6d777dcbbdb3f399452641
fidel <fidel@kali>
2024-05-25 21:12:18 +0300

    Added Persistent storage

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 function addTask() {
-    var taskInput = document.getElementById('taskInput');
-    var taskList = document.getElementById('taskList');
-    var newTaskDiv = document.createElement('div');
-    taskList.appendChild(newTaskDiv);
-    newTaskDiv.id = 'task' + (taskList.childElementCount + 1);
-    newTaskDiv.innerHTML = "<span id='task'>" + taskInput.value + "</span> <button onclick='deleteTask(\"" + newTaskDiv.id + "\")'>Delete</button>";
-    taskInput.value = "";
-  }
+  var taskInput = document.getElementById('taskInput');
+  var taskList = document.getElementById('taskList');
+  var newTaskDiv = document.createElement('div');
+  var taskId = 'task' + (taskList.childElementCount + 1);
+  newTaskDiv.id = taskId;
+  newTaskDiv.innerHTML = "<span id='task'>" + taskInput.value + "</span> <button onclick='deleteTask(\"" + taskId + "\")'>Delete</button>";
+  taskList.appendChild(newTaskDiv);
   
-  function deleteTask(taskId) {
-    var task = document.getElementById(taskId);
-    task.parentNode.removeChild(task);
-  }
+  // Save task to localStorage
+  var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  tasks.push(taskInput.value);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  
+  taskInput.value = "";
+}
+
+function deleteTask(taskId) {
+  var task = document.getElementById(taskId);
+  task.parentNode.removeChild(task);
+  
+  // Remove task from localStorage
+  var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  var index = parseInt(taskId.slice(4)) - 1; // Extracting task index from taskId
+  tasks.splice(index, 1);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Function to load tasks from localStorage on page load
+window.onload = function() {
+  var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  var taskList = document.getElementById('taskList');
+  tasks.forEach(function(task, index) {
+      var taskId = 'task' + (index + 1);
+      var newTaskDiv = document.createElement('div');
+      newTaskDiv.id = taskId;
+      newTaskDiv.innerHTML = "<span id='task'>" + task + "</span> <button onclick='deleteTask(\"" + taskId + "\")'>Delete</button>";
+      taskList.appendChild(newTaskDiv);
+  });
+};
